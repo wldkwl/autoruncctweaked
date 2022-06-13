318907078798e6ceee0673dbec283f1a0028360b
wldkwl <107405917+wldkwl@users.noreply.github.com>
2022-06-13 18:00:27 +0300

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,4 +1,4 @@
-shell.run("cd /")
-shell.run("wget https://pastebin.com/raw/PS5hkA8h")
-shell.run("rename PS5hkA8h startup.lua")
-shell.run("delete load.lua")
+os.run("cd /")
+os.run("wget https://pastebin.com/raw/PS5hkA8h")
+os.run("rename PS5hkA8h startup.lua")
+os.run("delete load.lua")
